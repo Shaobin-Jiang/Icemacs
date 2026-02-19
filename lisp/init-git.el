28187fc4984cc2edfc69e38f185cb3f58833c65a
Shaobin-Jiang <shaobin-jiang@outlook.com>
2026-02-19 22:03:27 +0800

    feat: improve magit commit
    These are just bundled together so I will just include them in one
    commit:
    
    - initial evil state should be 'insert when commiting
    - fixes the C-c C-k error when aborting a commit

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -31,7 +31,19 @@
   :config
   (setopt magit-format-file-function #'magit-format-file-nerd-icons)
   (setq magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1)
-  (evil-define-key 'normal magit-status-mode-map (kbd "q") 'magit-bury-or-kill-buffer))
+  (evil-set-initial-state 'git-commit-mode 'insert)
+  (evil-define-key 'normal magit-status-mode-map
+	(kbd "q") 'magit-bury-or-kill-buffer)
+  (evil-define-key '(normal insert) with-editor-mode-map
+	;; Default C-c C-k throws weird error to which I have no solution.
+	;; However, commit with C-c C-c, which calls `with-editor-finish'
+	;; works fine. Also, commiting an empty message will not actually
+	;; commit anything.
+	;; This does affect not only git commit, but all other with-editor
+	;; mode buffers as well, but they still have ZQ.
+	(kbd "C-c C-k") (lambda () (interactive)
+					  (delete-region (point-min) (point-max))
+					  (with-editor-finish t))))
 
 (provide 'init-git)
 
