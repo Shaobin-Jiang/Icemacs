0540b90e0e192ea74589d83873a34115a5bfef8f
Shaobin-Jiang <shaobin-jiang@outlook.com>
2026-03-19 15:26:17 +0800

    feat: use C-SPC as ESC in vterm

--- lisp/init-term.el
+++ lisp/init-term.el
@@ -12,7 +12,12 @@
   :straight t
   :defer t
   :hook (vterm-mode . (lambda () (interactive)
-						(display-line-numbers-mode -1))))
+						(display-line-numbers-mode -1)))
+  :config
+  (add-hook 'vterm-mode-hook (lambda ()
+							   (interactive)
+							   (evil-define-key 'insert vterm-mode-map
+								 (kbd "C-SPC") 'vterm-send-escape))))
 
 (provide 'init-term)
 
